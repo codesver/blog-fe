55e9a7ec3e9186da2b7f8a6794b354780f712051
codesver <codesver@gmail.com>
2023-12-25 19:48:29 +0900

    feat: init header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
+import Header from "./semantics/Header";
+
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/semantics/Header.tsx
@@ -0,0 +1,26 @@
+import React, { useCallback } from "react";
+
+import "./Semantic.css";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const onClickBlogIcon = useCallback(() => {
+    navigate("/");
+  }, []);
+
+  return (
+    <header className="Header">
+      <div className="blog-icon">
+        <img src="/codesver.png" alt="codesver" width={30} height={30} onClick={onClickBlogIcon} />
+      </div>
+      <h2>Codesver's Developer Blog</h2>
+      <div className="blog-menu">
+        <button className="menu">Menu</button>
+      </div>
+    </header>
+  );
+};
+
+export default React.memo(Header);
