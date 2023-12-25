98a4c1a65218c023b1a455a4218ee70812b35f59
codesver <codesver@gmail.com>
2023-12-25 16:52:15 +0900

    feat: common axios rest request

---
+++ src/utils/rest.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+interface Request {
+  method: string;
+  resource: string;
+  params?: object;
+  data?: object;
+}
+
+interface Response {
+  success: boolean;
+  data: unknown;
+}
+
+const baseUrl: string = "https://localhost:8080";
+
+export const REST = async (request: Request) => {
+  const response: Response = {} as Response;
+
+  await axios
+    .request({
+      method: request.method,
+      url: `${baseUrl}${request.resource}`,
+      params: request.params,
+      data: request.data,
+    })
+    .then((res) => {
+      response.success = true;
+      response.data = res.data;
+    })
+    .catch((err) => {
+      response.success = false;
+      response.data = err.response.data;
+    });
+
+  return response;
+};
