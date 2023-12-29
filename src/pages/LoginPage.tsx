3d3ce19dd6f75bbdb4c32572aac17f2ceea0af76
codesver <codesver@gmail.com>
2023-12-29 23:27:50 +0900

    feat: login page input and button section

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+
+import "./LoginPage.css"
 
 const LoginPage = () => {
-  return <div className="LoginPage"></div>;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const onChangeAccountInfo = (type: "ID" | "PW", info: string) => {
+    if (type === "ID") {
+      setUsername(info);
+    } else if (type === "PW") {
+      setPassword(info);
+    }
+  };
+
+  const onClickLoginButton = useCallback(() => {}, []);
+
+  const onClickGuestButton = useCallback(() => {}, []);
+
+  return (
+    <div className="LoginPage">
+      <div className="login-wrapper">
+        <div className="title">
+          HELLO
+        </div>
+        <div className="login-form" onSubmit={() => false}>
+          <div className="account-inputs">
+            <input type="text" className="account-input" placeholder="Username" maxLength={25} onChange={(e) => onChangeAccountInfo("ID", e.target.value)} />
+            <input type="password" className="account-input" placeholder="Password" maxLength={25} onChange={(e) => onChangeAccountInfo("PW", e.target.value)} />
+          </div>
+          <div className="account-buttons">
+            <button className="account-button" onClick={onClickLoginButton}>
+              Login
+            </button>
+            <button className="account-button" onClick={onClickGuestButton}>
+              Guest
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default React.memo(LoginPage);
--- src/semantics/header/Header.tsx
+++ src/semantics/header/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import "../Semantic.css";
 import Menu from "./Menu";
 
 const Header = () => {
