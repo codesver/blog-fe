6eff3793cf858275a3764d326b1f18a15c6c65b4
codesver <codesver@gmail.com>
2023-12-30 14:35:53 +0900

    feat: add animation b

---
+++ src/components/Welcome/animations/AnimationB.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./AnimationB.css"
+
+const AnimationB = () => {
+  return <div className="AnimationB">
+    <span></span>
+  </div>
+}
+
+export default React.memo(AnimationB)
