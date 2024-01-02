6f1cf817fe274763bef841965355875e87353f33
codesver <codesver@gmail.com>
2024-01-02 11:09:05 +0900

    fix: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<MainPage />} />
